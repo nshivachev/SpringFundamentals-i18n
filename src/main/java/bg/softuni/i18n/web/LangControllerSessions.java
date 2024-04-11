66333801cd6b6da58cd776ebe7d1819691a48531
nshivachev <shivachev.nikolay@gmail.com>
2024-04-11 18:56:54 +0300

    Implemented LangControllerCookies, LangControllerSessions

---
+++ src/main/java/bg/softuni/i18n/web/LangControllerSessions.java
@@ -0,0 +1,38 @@
+package bg.softuni.i18n.web;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LangControllerSessions {
+
+    @GetMapping("/sessions")
+    public String langCookies(
+            HttpSession session,
+            Model model) {
+
+        var lang = session.getAttribute("lang");
+
+        if (lang == null) {
+            lang = "bg";
+        }
+
+        model.addAttribute("lang", lang);
+
+        return "sessions";
+    }
+
+    @PostMapping("/sessions")
+    public String langCookies(@RequestParam("lang") String lang,
+                              HttpSession session) {
+
+        session.setAttribute("lang", lang);
+
+        return "redirect:/sessions";
+    }
+
+}
